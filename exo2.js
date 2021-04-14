99e48ae10066bf8c652063338b62b7f441be3c73
Janainaf <figueira.janaina@gmail.com>
2021-04-14 14:47:11 +0200

    modifc exo3

--- exo2.js
+++ exo2.js
@@ -1,27 +1,26 @@
 exo2 = (numbers) => {
-    let count = 0;
-	let result = [];
+  let count = 0;
+  let result = [];
 
-	for (let i = 0; i < numbers.length; i++) {   // para cada predio/numero do array numbers
-		for (let j = i + 1; j < numbers.length; j++) { // comparando com o predio ao seu lado
-            count++;
-			
-			
-			while (numbers[i] < numbers[j]) { // se o primeiro predio for menor que o segundo predio
-				result.push(j); // adicionar o predio maior
+  for (let i = 0; i < numbers.length; i++) {
+    // for (let j = i + 1; j < numbers.length; j++) {
+    count++;
 
-				console.log(`These are the buildings that watch the sunset ${result}`);  
-				console.log(`Bubble sort made ${count} comparisons`);
-				
-				return result // quero saber pq tem que ter um result aqui
-			   } 
-			   
-		   }
-	   }
-	   if (result === undefined);
-	   console.log(`Something went wrong `);
-   
-	   // Questao 2 - como passar o parametro target utilizando o node, o script.js e a list.txt?
-   }
+    if (numbers[i] < numbers[i + 1]) {
+      if (numbers[i + 1] > result[result.length - 1]) {
+        result.pop();
+      }
+      result.push(numbers[i + 1]);
+    }
+  }
+  result.push(numbers[numbers.length - 1]);
 
-   console.log(exo2([1, 5, 8, 5]));
+  if (result.length > 0) {
+    console.log(`These are the buildings that watch the sunset ${result}`);
+    console.log(`Bubble sort made ${count} comparisons`);
+  } else {
+    console.log(`somethin went wrong}`);
+  }
+};
+
+exo2([1, 5, 20, 8, 3, 10, 3, 6]);
