75c46e5284c359de3804084780a49753d01d55a1
Janainaf <figueira.janaina@gmail.com>
2021-04-14 23:56:30 +0200

    fixing exos adding exo4

--- exo1.js
+++ exo1.js
@@ -1,25 +1,25 @@
 exo1 = (numbers, target) => {
-	let result = [];
-    let count = 0;
+  let result = [];
+  let count = 0;
 
-	for (let i = 0; i < numbers.length; i++) { 
-		for (let j = i + 1; j < numbers.length; j++) {
-            count++;
-			if (numbers[i] + numbers[j] === target) {
-				result.push(i);
-				result.push(j);
-             console.log(`These are the numbers ${numbers[i]} and ${numbers[j]} that added gives us target number ${target} `);  
-             console.log(`It was made ${count} comparisons`);
-                  }
-		}
-	}
-    if (result.length > 0){
-        return result
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      count++;
+      if (numbers[i] + numbers[j] === target) {
+        result.push(i);
+        result.push(j);
+        console.log(
+          `These are the numbers ${numbers[i]} and ${numbers[j]} that added gives us target number ${target} `
+        );
+        console.log(`It was made ${count} comparisons`);
+      }
     }
-    else {
-    console.log(`There are no possible combination that adds to ${target}`)}
-}
-    
-exo1([4, 5, 8, 1], 9);
+  }
+  if (result.length > 0) {
+    return result;
+  } else {
+    console.log(`There are no possible combination that adds to ${target}`);
+  }
+};
 
-// Questao 2 - como passar o parametro target utilizando o node, o script.js e a list.txt?
+exo1([4, 5, 8, 1], 9);
--- exo4.js
+++ exo4.js
@@ -1,23 +1,16 @@
 exo4 = (numbers) => {
-  let count = 0;
-  let result = [];
-  let currentmax = 0;
+  let count = 1;
+  numbers = numbers.reverse();
 
-  for (let i = 0; i < numbers.length - 1; i++) {
-    count++;
-
-    if (numbers[i] > numbers[i + 1] && numbers[i] >= currentmax) {
-      result.push(numbers[i]);
+  let currentmax = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > currentmax || numbers[i] == currentmax) {
+      count++;
       currentmax = numbers[i];
     }
   }
-  result.push(numbers[numbers.length - 1]);
-  if (result.length > 0) {
-    console.log(`These are the buildings that watch the sunset ${result}`);
-    console.log(`Algo made ${count} comparisons`);
-  } else {
-    console.log(`Something went wrong`);
-  }
+  console.log(count);
+  return count;
 };
 
-exo4([50, 20, 70, 3, 10, 6, 5, 50]);
+exo4([5, 20, 10, 3, 10, 6, 50]);
