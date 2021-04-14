4a895f67222a0fb91732dc28f26479196489285a
Janainaf <figueira.janaina@gmail.com>
2021-04-14 13:10:43 +0200

    fixing exo1

--- exo1.js
+++ exo1.js
@@ -2,23 +2,24 @@ exo1 = (numbers, target) => {
 	let result = [];
     let count = 0;
 
-	for (let i = 0; i < numbers.length; i++) {
+	for (let i = 0; i < numbers.length; i++) { 
 		for (let j = i + 1; j < numbers.length; j++) {
             count++;
 			if (numbers[i] + numbers[j] === target) {
 				result.push(i);
 				result.push(j);
              console.log(`These are the numbers ${numbers[i]} and ${numbers[j]} that added gives us target number ${target} `);  
-             console.log(`Bubble sort made ${count} comparisons`);
-             return result // quero saber pq tem que ter um result aqui
-            } 
+             console.log(`It was made ${count} comparisons`);
+                  }
 		}
 	}
-    if (result === undefined);
-    console.log(`There are no possible combination that adds to ${target}  `);
-
-    // Questao 2 - como passar o parametro target utilizando o node, o script.js e a list.txt?
+    if (result.length > 0){
+        return result
+    }
+    else {
+    console.log(`There are no possible combination that adds to ${target}`)}
 }
-console.log(exo1([1, 5], 9));
+    
+exo1([4, 5, 8, 1], 9);
 
-module.exports = exo1;
+// Questao 2 - como passar o parametro target utilizando o node, o script.js e a list.txt?
